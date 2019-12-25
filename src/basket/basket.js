5cdaaca15124fd89f6eda6cedc0ad33273a6b494
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2019-12-25 23:04:07 +0300

    general structure of basket.js

---
+++ src/basket/basket.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+
+export class Basket extends Component {
+    state = {
+
+    }
+
+    render () {
+        return (
+            <div>Welcome to Basket</div>
+        )
+    }
+}
+
+export default Basket;
