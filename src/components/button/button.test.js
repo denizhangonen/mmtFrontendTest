2e74cbaba0e52d21eace392bd213fd2c71f18056
Denizhan Gönen <denizhangonen@Denizhans-MacBook-Pro.local>
2019-12-30 00:22:02 +0300

    tests for button has been added

--- src/components/button/button.test.js
+++ src/components/button/button.test.js
@@ -1,28 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import {configure,shallow} from 'enzyme';
-import Adapter from 'enzyme-adapter-react-16';
+import { configure, shallow } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
 
-import Button from './button'
+import Button from "./button";
 
-configure({adapter: new Adapter()});
+configure({ adapter: new Adapter() });
 
 describe("<Button /> component tests", () => {
-    let wrapper;
-    beforeEach(() => {
-        wrapper = shallow(<Button />)
-    })
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallow(<Button />);
+  });
 
-    it("should contain <button> element", () => {
-        expect(wrapper.find("button").exists()).toBe(true);
-    });
+  it("should contain <button> element", () => {
+    expect(wrapper.find("button").exists()).toBe(true);
+  });
 
-    // it("should render given children", () => {
-    //     wrapper.setProps({children: 'test'});
-    //     const wrapper2 = shallow(<Button>Test</Button>)
-    //     console.log(wrapper)
-    //     console.log(wrapper2)
-    //     expect(wrapper2.find("test").exists).toBe(true);
-    // })
-
-})
+  it("should render given children", () => {
+    const checkout = "Checkout";
+    wrapper.setProps({ children: checkout });
+    expect(wrapper.props().children).toEqual(checkout);
+  });
+});
